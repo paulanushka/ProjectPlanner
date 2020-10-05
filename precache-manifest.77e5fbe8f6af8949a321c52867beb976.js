7049a1c4745935bd3181f2044afabff1d9d61b99
Anushka Paul <anushkapaul1103@gmail.com>
2020-10-05 19:11:43 +0530

    Updates

---
+++ precache-manifest.77e5fbe8f6af8949a321c52867beb976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe9c39ffa7c3a3b43fea51a191ceaee",
+    "url": "/ProjectPlanner/index.html"
+  },
+  {
+    "revision": "808b2438959b54315b48",
+    "url": "/ProjectPlanner/static/css/main.53aa6dca.chunk.css"
+  },
+  {
+    "revision": "ba9c03ea55fc3104058d",
+    "url": "/ProjectPlanner/static/js/2.d7352aae.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/ProjectPlanner/static/js/2.d7352aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808b2438959b54315b48",
+    "url": "/ProjectPlanner/static/js/main.eb92857b.chunk.js"
+  },
+  {
+    "revision": "8bcc08eef2ed0a25d76a",
+    "url": "/ProjectPlanner/static/js/runtime-main.a259a911.js"
+  }
+]);
